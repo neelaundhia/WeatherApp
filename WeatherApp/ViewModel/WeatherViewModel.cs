46904edb331310cd9f9220b0692dfe0f9df6bc66
Neel Aundhia <neelaundhia@gmail.com>
2020-07-09 19:37:15 +0530

    Adding WeatherViewModel

---
+++ WeatherApp/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherViewModel: INotifyPropertyChanged
+    {
+        private string _query;
+
+        public string Query
+        {
+            get { return _query; }
+            set 
+            { 
+                _query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
