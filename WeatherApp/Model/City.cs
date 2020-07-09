2da8b97826cfd93f7b1da625b7c208032c29b061
Neel Aundhia <neelaundhia@gmail.com>
2020-07-09 15:57:14 +0530

    Added City and CurrentConditions classes to the Model

---
+++ WeatherApp/Model/City.cs
@@ -0,0 +1,20 @@
+﻿namespace WeatherApp.Model
+{
+    public class Area
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+
+    public class City
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public Area Country { get; set; }
+        public Area AdministrativeArea { get; set; }
+    }
+
+}
